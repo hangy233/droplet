9deb22a45a8079cf69d21338bf272a76474e29cf
Hang Ye <hangye@google.com>
2023-09-10 23:36:03 -0700

    brush

--- js/Game.js
+++ js/Game.js
@@ -153,8 +153,8 @@ export default class Game {
 		for (const type of Object.values(Piece.Type)) {
 			if (type === Piece.Type.DROPLET) {
 				for (const form of Object.values(Droplet.Form)) {
-					this.createBrush(type, form, Droplet.status.NORMAL, true);
-					this.createBrush(type, form, Droplet.status.NORMAL, false);
+					this.createBrush(type, form, Droplet.Status.NORMAL, true);
+					this.createBrush(type, form, Droplet.Status.NORMAL, false);
 				}
 			} else {
 				this.createBrush(type);
